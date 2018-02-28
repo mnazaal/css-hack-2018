d99eba375001e65f5e55d5c9710b8a542d15c8a7
mnazaal <mnazaal@github.com>
2018-02-28 18:23:47 +0000

    Map should be visible now

--- ground-station/__init__.py
+++ ground-station/__init__.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+#from flask_googlemaps import GoogleMaps
+
 app = Flask(__name__)
 
+#app.config['GOOGLEMAPS_KEY'] = "8JZ7i18MjFuM35dJHq70n3Hx4"
+
+#GoogleMaps(app)
+
+#GoogleMaps(app, key="8JZ7i18MjFuM35dJHq70n3Hx4")
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template("index.html")
+
+
+
