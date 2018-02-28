6beda25d6378e3ff41933bea697969ef902b669a
mnazaal <mnazaal@github.com>
2018-02-28 20:53:57 +0000

    POST REQUEST RETAINS DATA IN PYTHON VAIRABLE

--- ground-station/__init__.py
+++ ground-station/__init__.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 #from flask_googlemaps import GoogleMaps
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
+
+api = Api(app)
 
 #app.config['GOOGLEMAPS_KEY'] = "8JZ7i18MjFuM35dJHq70n3Hx4"
 
@@ -13,5 +16,11 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-
-
+longitude = 51.508742
+latitude = -0.120850
+@app.route('/', methods = ['POST'])
+def PostHandler():
+    data = request.get_data()
+    return str(data)
+#class Signal(Resource):
+    
