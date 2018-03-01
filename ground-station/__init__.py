3e9433faf1bc9cc8ee31fb7b78d22feeb51b0c0a
mnazaal <mnazaal@github.com>
2018-03-01 11:39:31 +0000

    longitude and latitude -hack-

--- ground-station/__init__.py
+++ ground-station/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 #from flask_googlemaps import GoogleMaps
 from flask_restful import Resource, Api, reqparse
 import reverse_geocoder as rg
@@ -13,13 +13,15 @@ api = Api(app)
 
 #GoogleMaps(app, key="8JZ7i18MjFuM35dJHq70n3Hx4")
 
+loc = ""
+
 @app.route('/')
 def index():
     #data = request.get_data()
     #data = data.decode()
     #data = data.split("&")
     #latitude, longitude = data[5].split("=")[1], data[6].split("=")[1]
-    latitude, longitude = 51.508742, -0.120850
+    latitude, longitude = 51.455593, -2.603691
     loc = {"lat": latitude, "lng": longitude}
     return render_template("index.html", loc=loc)
    
@@ -32,12 +34,15 @@ def PostHandler():
     latitude, longitude = data[5].split("=")[1], data[6].split("=")[1]
     print(latitude)
     print(longitude)
+    global loc
     loc = {"lat": latitude, "lng": longitude}
     locextra = rg.search((latitude, longitude))[0]
     area = locextra['name']
     country = locextra['admin1']
     print(str(area + ', ' + country))
-    return render_template("index.html", loc=loc)
+    print("===")
+    print(loc)
+    return jsonify(loc=loc)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=4040,  debug=True)
