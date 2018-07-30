07cf51c6ae7fe2fe9e278a25e4aa53943c556efc
Mohamed Nazaal Ibrahim <mnazaal@users.noreply.github.com>
2018-07-30 18:36:04 +0100

    Update __init__.py

--- ground-station/__init__.py
+++ ground-station/__init__.py
@@ -7,11 +7,9 @@ app = Flask(__name__)
 
 api = Api(app)
 
-#app.config['GOOGLEMAPS_KEY'] = "8JZ7i18MjFuM35dJHq70n3Hx4"
 
 #GoogleMaps(app)
 
-#GoogleMaps(app, key="8JZ7i18MjFuM35dJHq70n3Hx4")
 
 loc = ""
 
